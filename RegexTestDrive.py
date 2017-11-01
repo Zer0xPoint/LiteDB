c5a258aa519903f5876ab447a64c1c0d9fcae527
rileylee <liyizhou@me.com>
2017-11-02 00:53:46 +0800

    add func create_new_table to Table.py

--- RegexTestDrive.py
+++ RegexTestDrive.py
@@ -6,10 +6,14 @@ def get_command():
     command = '''aosdfiulakwnelkfihas;dfj(name char(10) not null, ssn char(18) not null, bdate char(10) not null, address char(30) not null, sex char(2) not null, salary float not null, superssn char(18) not null, dno char(3) not null, primary key(ssn))'''
     # command_parse = re.search(r"\((?:[^{}]|(?R))*\)", command)
     # print(command_parse.group())
-    command_parse = regex.compile(r"\((?:[^{}]|(?R))*\)")
-    fnd = command_parse.findall(command)
-    for j in fnd:
-        print(j)
+    # command_parse = regex.compile(r"\((?:[^{}]|(?R))*\)")
+    # fnd = command_parse.findall(command)
+    # for j in fnd:
+    #     print(j)
+    command_parse = regex.search(r"\((?:[^{}]|(?R))*\)", command)
+    table_attribs = command_parse.group().split(",")
+    print(table_attribs)
+
 
 if __name__ == "__main__":
     get_command()
--- Tables.py
+++ Tables.py
@@ -1,5 +1,8 @@
 import re
+import regex
 
 
 def create_new_table(command):
-    command_parse = re.search()
+    command_parse = re.search(r"\((?:[^{}]|(?R))*\)", command)
+    table_attribs = command_parse.group()
+    print(table_attribs)
