62f492596c639622bad84565f691496b357c21b7
rileylee <liyizhou@me.com>
2017-11-04 21:12:10 +0800

    divide table_info_list to two attribs and show it with table

--- Tables.py
+++ Tables.py
@@ -7,17 +7,39 @@ def create_new_table(command):
     table_name_parse = re.search(r"create\s*?table\s(.*)", command)
     table_name = table_name_parse.group(1)
     command = command.replace("create table test", "")
-    table_info_parse = regex.search(r"\((?:[^{}]|(?R))*\)", command)
-    table_infos = table_info_parse.group()
-    table_infos = str(table_infos)[1:-1].split(",")
-    table_field = table_infos[0].split(" ")[0]
 
+    table_info_parse = regex.search(r"\((?:[^{}]|(?R))*\)", command)
+    table_info_group = table_info_parse.group()
+    table_info_list = str(table_info_group)[1:-1].split(",")
+
+    table_field, table_type = [], []
 
     show_table = PrettyTable(["Field", "Type"])
-    print(table_infos)
+
+    for i in range(len(table_info_list)):
+        table_attrib_list = str(table_info_list[i]).split(" ")
+        print(table_attrib_list)
+        show_table.add_row(table_attrib_list)
+
+        # for x in range(len(table_attrib_list)):
+        #     table_field.append(table_attrib_list[0])
+        #     table_type.append(table_attrib_list[1])
+        # show_table.add_row([table_field[x], table_type[x]])
+
+        # print(table_field, table_type)
+    # print(table_attrib_list)
+    # for i in table_infos:
+    #     print(table_infos[i].split(" ")[0])
+    #
+    # table_field = table_infos[0].split(" ")[0]
+    # table_type = table_infos[0].split(" ")[1]
+    #
+    #
+    # print(table_infos)
     print(show_table)
-    print(table_field)
+    # print(table_field, table_type)
+
 
 if __name__ == "__main__":
-    command = "create table test(name varchar(20), id int)"
+    command = "create table test(name varchar(20),id int,birth int,salary int)"
     create_new_table(command)
